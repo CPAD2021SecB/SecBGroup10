3d9428ded16edf92b965520870d6453fc5afa8c3
Amit Kumar <46594792+ak672676@users.noreply.github.com>
2021-12-11 12:15:06 +0530

    Splash Screen and Login Provider Added

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,85 @@
+import 'dart:async';
+
+import 'package:breview/pages/home_page.dart';
+import 'package:breview/pages/login_page.dart';
+import 'package:breview/provider/LoginProvider.dart';
+import 'package:breview/util/RouteAnimation.dart';
+import "package:flutter/material.dart";
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  StreamSubscription _streamSubscription;
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    LoginProvider.instantiate();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Timer(Duration(milliseconds: 1000), () {
+      checkLoginStatus(_scaffoldKey.currentContext);
+    });
+
+    return Scaffold(
+      key: _scaffoldKey,
+      backgroundColor: Colors.black,
+      body: SafeArea(
+          child: Align(
+              alignment: Alignment.center,
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      "assets/images/splash.png",
+                      fit: BoxFit.scaleDown,
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      "Breview",
+                      style: TextStyle(
+                          fontSize: 40,
+                          fontFamily: "Montserrat",
+                          fontWeight: FontWeight.w600,
+                          color: Colors.amber[800]),
+                    ),
+                  ],
+                ),
+              ))),
+    );
+  }
+
+  void checkLoginStatus(BuildContext Navcontext) {
+    _streamSubscription = LoginProvider.stateStream.listen((state) {
+      if (state == PhoneAuthState.Failed || state == PhoneAuthState.newUser) {
+        Future.delayed(Duration(seconds: 1), () {
+          Navigator.of(_scaffoldKey.currentContext)
+              .pushReplacement(FadeRoute(page: PhoneloginWidget()));
+        });
+      }
+      if (state == PhoneAuthState.Verified) {
+        Future.delayed(Duration(seconds: 1), () {
+          Navigator.of(Navcontext).pushReplacement(FadeRoute(page: HomePage()));
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _streamSubscription.cancel();
+    _streamSubscription = null;
+    super.dispose();
+  }
+}
