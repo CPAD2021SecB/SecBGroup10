b20f0672329161520ee0cf63c7b5fe9f37a277d3
Abin Babu <abin.babu01@sap.com>
2021-12-11 11:19:08 +0530

    firebase web refactoring

--- breview/lib/main.dart
+++ breview/lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:breview/pages/create_blog_ui.dart';
+import 'package:breview/pages/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: "AIzaSyACUL935VNkFDUp_PpFakoRPxEbGMNuh4s",
+          authDomain: "breview-334103.firebaseapp.com",
+          projectId: "breview-334103",
+          storageBucket: "breview-334103.appspot.com",
+          messagingSenderId: "934338543057",
+          appId: "1:934338543057:web:25b0df1edb3072fad4b4f0",
+          // measurementId: "${config.measurementId}"
+      ));
   runApp(MyApp());
 }
 
@@ -17,7 +27,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         brightness: Brightness.dark,
       ),
-      home: CreateBlog(),
+      home: HomePage(),
     );
   }
 }
