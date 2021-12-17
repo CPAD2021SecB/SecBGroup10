64889a1afd9ae967a37ae46e7dfa4d03fdd7bab2
Abin Babu <abin.babu01@sap.com>
2021-12-17 20:33:03 +0530

    ui refactoring

--- breview/lib/pages/friend_profile_page.dart
+++ breview/lib/pages/friend_profile_page.dart
@@ -1,6 +1,8 @@
 import 'package:breview/services/crud.dart';
 import 'package:breview/widgets/BlogsWidget.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_frame/flutter_web_frame.dart';
 
 class FriendProfile extends StatefulWidget {
   FriendProfile({Key key}) : super(key: key);
@@ -16,172 +18,188 @@ class _FriendProfileState extends State<FriendProfile> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      backgroundColor: Colors.black,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print('FloatingActionButton pressed ...');
-        },
-        backgroundColor: Color(0xFFB39738),
-        elevation: 8,
-        child: Icon(
-          Icons.create_rounded,
-          color: Colors.white,
-          size: 24,
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                color: Colors.black,
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 3,
-                    color: Color(0x3A000000),
-                    offset: Offset(0, 1),
-                  )
-                ],
-              ),
-              child: Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(0, 44, 0, 0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(16, 0, 0, 0),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          Padding(
-                            padding: EdgeInsetsDirectional.fromSTEB(0, 0, 8, 0),
-                            child: Container(
-                              width: 40,
-                              height: 40,
-                              clipBehavior: Clip.antiAlias,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                              ),
-                              child: Image.network(
-                                'https://i.picsum.photos/id/1074/5472/3648.jpg?hmac=w-Fbv9bl0KpEUgZugbsiGk3Y2-LGAuiLZOYsRk0zo4A',
-                                fit: BoxFit.fitHeight,
-                              ),
-                            ),
-                          ),
-                          Text(
-                            'Rajat Ranjan',
-                            style: TextStyle(
-                              fontFamily: 'Lexend Deca',
-                              color: Colors.white,
-                              fontSize: 22,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(16, 0, 0, 12),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          Padding(
-                            padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
-                            child: Text(
-                              'See my favorite picks.',
-                              style: TextStyle(
-                                fontFamily: 'Lexend Deca',
-                                color: Color(0xFF8B97A2),
-                                fontSize: 14,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    )
-                  ],
-                ),
+    return FlutterWebFrame(
+        maximumSize: Size(750, double.infinity),
+        enabled: kIsWeb && MediaQuery.of(context).size.width > 700,
+        backgroundColor: Colors.black,
+        builder: (context) {
+          return Scaffold(
+            key: scaffoldKey,
+            backgroundColor: Colors.black,
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                print('FloatingActionButton pressed ...');
+              },
+              backgroundColor: Color(0xFFB39738),
+              elevation: 8,
+              child: Icon(
+                Icons.create_rounded,
+                color: Colors.white,
+                size: 24,
               ),
             ),
-
-            Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 32),
+            body: SingleChildScrollView(
               child: Column(
                 mainAxisSize: MainAxisSize.max,
                 children: [
-                  FutureBuilder(
-                    future: crudMethods.getData("blogs"),
-                    builder: (context,AsyncSnapshot<dynamic> snap) {
-                    return Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 32),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          StreamBuilder(
-                            stream: snap.data,
-                            builder: (context, snapshot){
-                              if(snapshot.hasData){
-                                return ListView.builder( physics: NeverScrollableScrollPhysics(),
-                                  shrinkWrap: true,
-                                  itemCount: snapshot.data.docs.length,
-                                  itemBuilder: (context, index){
-                                    return BlogsWidget(
-                                      image: snapshot.data.docs[index]['image'],
-                                      likes: snapshot.data.docs[index]['likes'].toString()
-                                    );
-                                  });
-                              }
-                              else{
-                                return Container(
-                                  alignment: Alignment.center,
-                                  child: CircularProgressIndicator()
-                                );
-                              }
-                            }),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 3,
+                          color: Color(0x3A000000),
+                          offset: Offset(0, 1),
+                        )
                       ],
                     ),
-                  );
-                }),
-                  Padding(
-                    padding: EdgeInsetsDirectional.fromSTEB(0, 8, 0, 12),
-                    child: Container(
-                      width: MediaQuery.of(context).size.width * 0.94,
-                      decoration: BoxDecoration(),
+                    child: Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 44, 0, 0),
                       child: Column(
                         mainAxisSize: MainAxisSize.max,
                         children: [
                           Padding(
-                            padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
-                            child: Container(
-                              width: MediaQuery.of(context).size.width * 0.96,
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                boxShadow: [
-                                  BoxShadow(
-                                    blurRadius: 6,
-                                    color: Color(0x3A000000),
-                                    offset: Offset(0, 2),
-                                  )
-                                ],
-                                borderRadius: BorderRadius.circular(8),
-                              ),
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(16, 0, 0, 0),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                Padding(
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      0, 0, 8, 0),
+                                  child: Container(
+                                    width: 40,
+                                    height: 40,
+                                    clipBehavior: Clip.antiAlias,
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                    ),
+                                    child: Image.network(
+                                      'https://i.picsum.photos/id/1074/5472/3648.jpg?hmac=w-Fbv9bl0KpEUgZugbsiGk3Y2-LGAuiLZOYsRk0zo4A',
+                                      fit: BoxFit.fitHeight,
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  'Rajat Ranjan',
+                                  style: TextStyle(
+                                    fontFamily: 'Lexend Deca',
+                                    color: Colors.white,
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(16, 0, 0, 12),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                Padding(
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      0, 4, 0, 0),
+                                  child: Text(
+                                    'See my favorite picks.',
+                                    style: TextStyle(
+                                      fontFamily: 'Lexend Deca',
+                                      color: Color(0xFF8B97A2),
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                )
+                              ],
                             ),
                           )
                         ],
                       ),
                     ),
                   ),
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 32),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        FutureBuilder(
+                            future: crudMethods.getData("blogs"),
+                            builder: (context, AsyncSnapshot<dynamic> snap) {
+                              return Padding(
+                                padding:
+                                    EdgeInsetsDirectional.fromSTEB(0, 0, 0, 32),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: [
+                                    StreamBuilder(
+                                        stream: snap.data,
+                                        builder: (context, snapshot) {
+                                          if (snapshot.hasData) {
+                                            return ListView.builder(
+                                                physics:
+                                                    NeverScrollableScrollPhysics(),
+                                                shrinkWrap: true,
+                                                itemCount:
+                                                    snapshot.data.docs.length,
+                                                itemBuilder: (context, index) {
+                                                  return BlogsWidget(
+                                                      image: snapshot.data
+                                                          .docs[index]['image'],
+                                                      likes: snapshot.data
+                                                          .docs[index]['likes']
+                                                          .toString());
+                                                });
+                                          } else {
+                                            return Container(
+                                                alignment: Alignment.center,
+                                                child:
+                                                    CircularProgressIndicator());
+                                          }
+                                        }),
+                                  ],
+                                ),
+                              );
+                            }),
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(0, 8, 0, 12),
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 0.94,
+                            decoration: BoxDecoration(),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                Padding(
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      0, 4, 0, 0),
+                                  child: Container(
+                                    width: MediaQuery.of(context).size.width *
+                                        0.96,
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      boxShadow: [
+                                        BoxShadow(
+                                          blurRadius: 6,
+                                          color: Color(0x3A000000),
+                                          offset: Offset(0, 2),
+                                        )
+                                      ],
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
                 ],
               ),
-            )
-          ],
-        ),
-      ),
-    );
+            ),
+          );
+        });
   }
 }
